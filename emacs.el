bf4167bd3127055c566ceccb8a5949aa56795595
deko <dkolokol@gmail.com>
2019-04-18 13:16:53 +0200

    add yaml-mode to emacs

--- emacs.el
+++ emacs.el
@@ -1,6 +1,8 @@
-; Emacs customisations
-; Copy this to /home/$USER/.emacs
+;;; package --- Emacs customisations
 
+;; Warning: copy this to /home/$USER/.emacs
+
+;; Copy to X-clipboard 
 (defun my-copy-to-xclipboard(arg)
   (interactive "P")
   (cond
@@ -40,3 +42,102 @@ pastes from X-SECONDARY."
 (global-set-key (kbd "C-c C-w") 'my-cut-to-xclipboard)
 (global-set-key (kbd "C-c M-w") 'my-copy-to-xclipboard)
 (global-set-key (kbd "C-c C-y") 'my-paste-from-xclipboard)
+
+
+;; ELPA
+;; source in ~/.emacs.d/elpa
+;; Requisites: Emacs >= 24
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(package-refresh-contents)
+
+(defun install-if-needed (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; make more packages available with the package installer
+(defvar to-install)
+(setq to-install
+      '(python-mode magit yasnippet jedi auto-complete autopair find-file-in-repository flycheck exec-path-from-shell yaml-mode))
+
+(mapc 'install-if-needed to-install)
+
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(require 'auto-complete)
+(require 'autopair)
+(require 'yasnippet)
+(require 'flycheck)
+(require 'exec-path-from-shell)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell flycheck find-file-in-repository autopair jedi yasnippet magit python-mode yaml-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(global-flycheck-mode t)
+
+(global-set-key [f7] 'find-file-in-repository)
+
+; auto-complete mode extra settings
+(setq
+ ac-auto-start 2
+ ac-override-local-map nil
+ ac-use-menu-map t
+ ac-candidate-limit 20)
+
+;; Python mode settings
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(setq py-electric-colon-active t)
+(add-hook 'python-mode-hook 'autopair-mode)
+(add-hook 'python-mode-hook 'yas-minor-mode)
+
+;; Jedi settings
+(require 'jedi)
+;; It's also required to run "pip install --user jedi" and "pip
+;; install --user epc" to get the Python side of the library work
+;; correctly.
+;; With the same interpreter you're using.
+
+;; if you need to change your python intepreter, if you want to change it
+;; (setq jedi:server-command
+;;       '("python2" "/home/andrea/.emacs.d/elpa/jedi-0.1.2/jediepcserver.py"))
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (jedi:setup)
+	    (jedi:ac-setup)
+            (local-set-key "\C-cd" 'jedi:show-doc)
+            (local-set-key (kbd "M-SPC") 'jedi:complete)
+            (local-set-key (kbd "M-.") 'jedi:goto-definition)))
+
+(add-hook 'python-mode-hook 'auto-complete-mode)
+
+(ido-mode t)
+
+;; YASnippets
+(yas-global-mode 1)
+
+;; YAML mode hook
+(require 'yaml-mode)
+(add-hook 'yaml-mode-hook
+          (lambda ()
+            (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;;; emacs.el ends here
